68ea974164a7f2dd11160aa51689df390daaf546
--add <168851515+nelly2040@users.noreply.github.com>
2024-12-16 21:43:21 +0300

    commit

--- Mancala /board.py
+++ Mancala /board.py
@@ -1,15 +1,5 @@
-class MancalaBoard:
-    def __init__(self):
-        # Initialize player's pits with 4 stones each and stores with 0
-        self.player1_pits = [4] * 6  # 6 pits for player 1
-        self.player2_pits = [4] * 6  # 6 pits for player 2
-        self.player1_store = 0        # Store for player 1
-        self.player2_store = 0        # Store for player 2
+# board_setup/initialize_board.py
 
-    def get_board_state(self):
-        return {
-            "player1": self.player1_pits,
-            "player1_store": self.player1_store,
-            "player2": self.player2_pits,
-            "player2_store": self.player2_store
-        }
+def initialize_board():
+    board = [4] * 6 + [0] + [4] * 6 + [0]
+    return board
