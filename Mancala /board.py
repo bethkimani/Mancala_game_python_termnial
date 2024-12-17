288c258b63c5475642e5cec5f6251af86711c42a
--add <168851515+nelly2040@users.noreply.github.com>
2024-12-17 15:49:24 +0300

    commit

--- Mancala /board.py
+++ Mancala /board.py
@@ -1,5 +1,10 @@
 # board_setup/initialize_board.py
 
 def initialize_board():
-    board = [4] * 6 + [0] + [4] * 6 + [0]
+    board = {
+        'player_1_pits': [4] * 6,
+        'player_1_mancala': 0,
+        'player_2_pits': [4] * 6,
+        'player_2_mancala': 0
+    }
     return board
