cb644e4243d55335b3c9c9af706e2cdcce7a614c
essie-kamau043 <156332730+essie-kamau043@users.noreply.github.com>
2024-12-18 12:15:24 +0300

    Added main.py

--- Mancala /board.py
+++ Mancala /board.py
@@ -1,27 +1,23 @@
-# display_board/display_board.py
-
 import random
 
-from AlternatePlayerTurn import check_game_over
 # Mancala Game Implementation
-
 
 binAmount = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 
 def display_board():
-    # Displaying the board with proper alignment
     print("+----+----+----+----+----+----+----+----+")
-    print("|    |", str(binAmount[12]).rjust(2), "|", str(binAmount[11]).rjust(2), "|", str(binAmount[10]).rjust(2), "|",
-          str(binAmount[9]).rjust(2), "|", str(binAmount[8]).rjust(2), "|", str(binAmount[7]).rjust(2), "|    |")
-    print("|", str(binAmount[13]).rjust(2), " |----+----+----+----+----+-----|", str(binAmount[6]).rjust(2), " |")
-    print("|    |", str(binAmount[0]).rjust(2), " |", str(binAmount[1]).rjust(2), "|", str(binAmount[2]).rjust(2), "|",
-          str(binAmount[3]).rjust(2), "|", str(binAmount[4]).rjust(2), "|", str(binAmount[5]).rjust(2), "|    |")
+    print("|    |" + str(binAmount[12]).rjust(2) + "    |" + str(binAmount[11]).rjust(2) + " |" + str(binAmount[10]).rjust(2) +
+          "  | " + str(binAmount[9]).rjust(2) + " |" + str(binAmount[8]).rjust(2) + " | " + str(binAmount[7]).rjust(2) + "   |    |")
+    print("|" + str(binAmount[13]).rjust(2) + "   |----+----+----+----+----+-----|" + str(binAmount[6]).rjust(2) + " |")
+    print("|    |  " + str(binAmount[0]).rjust(2) + " | " + str(binAmount[1]).rjust(2) + "  | " + str(binAmount[2]).rjust(2) +
+          "   | " + str(binAmount[3]).rjust(2) + "  | " + str(binAmount[4]).rjust(2) + " |" + str(binAmount[5]).rjust(2) + "   |    |")
     print("+----+----+----+----+----+----+----+----+")
     print("        f     e     d     c     b     a")
     print("")
+
 def distribute_stones(chosenBin, playerOne):
     global binAmount
-    giveawayPigit pull origin mainle = binAmount[chosenBin]
+    giveawayPile = binAmount[chosenBin]
     
     if giveawayPile == 0:
         return -1  # Invalid move, chosen bin is empty
@@ -54,7 +50,8 @@ def distribute_stones(chosenBin, playerOne):
         binAmount[12 - lastRecipient] = 0
     
     return lastRecipient
-    def check_game_over():
+
+def check_game_over():
     sideOne = sum(binAmount[0:6])
     sideTwo = sum(binAmount[7:13])
     return sideOne == 0 or sideTwo == 0
@@ -69,6 +66,7 @@ def get_computer_move():
 def main():
     playing = True
     playerOne = True
+
     while playing:
         display_board()
         if playerOne:
